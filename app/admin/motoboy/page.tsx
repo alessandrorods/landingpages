ad653ef0efd7f267a331d6475fb7d5ba3f3e3941
Alessandro (Rods) Rodrigues <33767174+alessandrorods@users.noreply.github.com>
2026-05-04 13:33:53 -0300

    Update page.tsx

--- app/admin/motoboy/page.tsx
+++ app/admin/motoboy/page.tsx
@@ -321,6 +321,7 @@ export default function MotoboyPage() {
           onClose={() => setAberto(null)}
           hideBuyer
           hidePrices
+          hideCardMessage
           action={
             <EntregaAction
               pedido={aberto}
